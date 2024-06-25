b826b8ae585b6857d52bc43db46c5d9a2a81fa77
MrMorby <die.acevedoc@duocuc.cl>
2024-06-24 20:54:23 -0400

    Aa

--- index.js
+++ index.js
@@ -26,13 +26,16 @@ const email = document.getElementById("email")
 const pass = document.getElementById("password")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
+const rut = document.getElementById("rut")
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
     let warnings = ""
     let entrar = false
+    let regexRut = /^[0-9]+[-|‐]{1}[0-9kK]{1}$/
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
     parrafo.innerHTML = ""
+
     if(nombre.value.length <4){
         warnings += `El nombre es muy corto <br>`
         entrar = true
@@ -43,6 +46,10 @@ form.addEventListener("submit", e=>{
     }
     if(pass.value.length < 8){
         warnings += `La contraseña es muy corta <br>`
+        entrar = true
+    }
+    if(!regexRut.test(rut.value)){
+        warnings += `El rut no es valido <br>`
         entrar = true
     }
 
